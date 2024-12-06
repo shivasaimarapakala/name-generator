d948495892b36d1f79a51b169b91029dcd7032ee
romitkarmakar <romitkarmakar@gmail.com>
2024-12-06 19:48:38 +0530

    Added /about endpoint

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/about', (req, res) => {
+  res.send('About Us')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
